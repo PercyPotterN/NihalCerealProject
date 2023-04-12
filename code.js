805a540f4d4950ad2d70402c5c3a1f5d0a597b7c
PercyPotterN <111822433+PercyPotterN@users.noreply.github.com>
2023-04-11 22:28:36 -0700

    Add files via upload

---
+++ code.js
@@ -0,0 +1,32 @@
+setScreen("screen1");
+
+onEvent("button1", "click", function( ) {
+  setScreen("screen2");
+});
+onEvent("button2", "click", function( ) {
+  open("https://www.walmart.com/ip/Imaginext-Shark-Bite-Pirate-Ship-Playset/49851356");
+});
+onEvent("button3", "click", function( ) {
+  open("https://www.target.com/p/playmobil-playmobil-pirates-70411-skull-pirate-ship-132-piece-set/-/A-85342405#lnk=sametab");
+});
+onEvent("button3", "click", function( ) {
+  setScreen("screen2");
+});
+onEvent("button4", "click", function( ) {
+  open("https://www.walmart.com/ip/LEGO-Miscellaneous-Pirates-and-Treasure-VIP-Add-On-Pack-40515/1807086493?from=searchResults");
+});
+onEvent("buttonAll", "click", function( ) {
+  setScreen("screen3");
+});
+onEvent("button5", "click", function( ) {
+  open("https://www.amazon.com/s?k=Pirate+Toys&crid=3TWK6J2BQVOCC&sprefix=pirate+toy%2Caps%2C159&ref=nb_sb_noss_1");
+});
+onEvent("button7", "click", function( ) {
+  open("https://www.walmart.com/search?q=Pirate%20toys");
+});
+onEvent("button6", "click", function( ) {
+  open("https://www.target.com/s?searchTerm=pirate+toys&tref=typeahead%257Cterm%257C0%257Cpirate%2520toys%257Cpirate%2520toys%257C%257C%257Cservice%257C10%257C%257C%257C&category=0%7CAll%7Cmatchallpartial%7Call+categories&searchTermRaw=");
+});
+onEvent("button8", "click", function( ) {
+  setScreen("screen2");
+});
